c6ec5707afac8aca4c1f313345d54eff92428966
David N. Mashburn <david.n.mashburn@gmail.com>
2012-08-18 17:22:35 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,18 @@ setup(
     description='',
     long_description=open('README.rst').read(),
     install_requires=[
+# Adding install requirements may make this quirk out on a new system
+# If it does, I'll have to patch it!
                       'wx>=2.8',
-                      'numpy>=0.9',
+                      'numpy>=1.0',
+                      'scipy>=0.8',
+                      'matplotlib>=1.0',
+                      'Image>=1.1.5',
+                      'mahotas>=0.5',
+#                      # For now, these are still housed internally...
+#                      'GifTiffLoader>=0.1',
+#                      'cmpGen>=0.1',
+#                      'ImageContour>=0.1',
+#                      'EllipseFitter>=0.1',
                       ],
 )
