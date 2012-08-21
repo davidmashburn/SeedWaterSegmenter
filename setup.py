985d3fe8c1c8db02b4c399869827c1da2e4b93a2
David N. Mashburn <david.n.mashburn@gmail.com>
2012-08-21 17:00:44 -0500

    Change these files to be external dependencies instead: cmpGen, EllipseFitter, FilenameSort, GifTiffLoader, and ImageContour

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SeedWaterSegmenter',
-    version='0.5.0',
+    version='0.6.0',
     author='David N. Mashburn',
     author_email='david.n.mashburn@gmail.com',
     packages=['SeedWaterSegmenter'],
@@ -20,10 +20,11 @@ setup(
                       'matplotlib>=1.0',
                       'Image>=1.1.5',
                       'mahotas>=0.5',
-#                      # For now, these are still housed internally...
-#                      'GifTiffLoader>=0.1',
-#                      'cmpGen>=0.1',
-#                      'ImageContour>=0.1',
-#                      'EllipseFitter>=0.1',
+                      # Projects that used to be internal
+                      'cmpGen>=0.1',
+                      'EllipseFitter>=0.1',
+                      'FilenameSort>=0.1',
+                      'GifTiffLoader>=0.1',
+                      'ImageContour>=0.1',
                       ],
 )
