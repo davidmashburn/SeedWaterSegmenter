78adeadb1a6760a41a14743684ddbfb1acca1c60
David N. Mashburn <david.n.mashburn@gmail.com>
2013-04-29 09:44:08 -0500

    Moved SubContourTools to the ImageContour module instead

--- SeedWaterSegmenter/__init__.py
+++ SeedWaterSegmenter/__init__.py
@@ -1,7 +1,7 @@
 # This should import everything relevant:
 from SeedWaterSegmenter import *
 import SWHelpers
-import SubContourTools
+from ImageContour import SubContourTools
 from _version import *
 
-__all__ = ['SeedWaterSegmenter','SWHelpers','SubContourTools']
+__all__ = ['SeedWaterSegmenter','SWHelpers']
