2d8d834068ba6a4dc935bf05fe49497992950a2b
David Mashburn <david.n.mashburn@gmail.com>
2022-02-19 12:01:47 -0500

    Bump package versions, add a requirements.txt file for future use, bump version to 0.6.7

--- setup.py
+++ setup.py
@@ -18,9 +18,7 @@ setup(
     description="graphical program to interactively segment image stacks of cells in tissue with edge-labels (aka. white outlines)",
     long_description=open("README.rst").read(),
     install_requires=[
-        # Adding install requirements may make this quirk out on a new system
-        # If it does, I'll have to patch it!
-        #'wxPython>=2.8', # wxPython isn't being found correctly by setuptools -- please install it manually!
+        "wxPython>=4.0",
         "numpy>=1.0",
         "scipy>=0.8",
         "matplotlib>=1.0",
@@ -30,9 +28,9 @@ setup(
         "mahotas>=0.5",
         #                      # Projects that used to be internal
         "EllipseFitter>=0.1.3",
-        "FilenameSort>=0.2.1",
+        "FilenameSort>=0.2.2",
         "GifTiffLoader>=0.2.4",
-        "ImageContour>=0.2.5",
+        "ImageContour>=0.2.7",
         "np_utils>=0.5.4",
     ],
 )
